4acb91224c77a6760dbd6369ef70340ff66a9152
zaidcoderboltehai <Pass@7488690>
2025-04-22 01:18:25 +0530

    [Zaid]:added debugging

--- Bookstore.API/obj/Debug/net6.0/Bookstore.API.AssemblyInfo.cs
+++ Bookstore.API/obj/Debug/net6.0/Bookstore.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8dbdb48856cb37a627412d1105a2dcb26f17c20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea02b2197f70ba83ed95928e2bbc072506d4acb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
