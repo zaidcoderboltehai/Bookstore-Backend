cd125aa1b3f80c10ac47e1010c4ad8ed4d639acc
zaidcoderboltehai <Pass@7488690>
2025-05-28 17:45:27 +0530

    [Zaid]added:rate limit

--- Bookstore.Business/obj/Debug/net6.0/Bookstore.Business.AssemblyInfo.cs
+++ Bookstore.Business/obj/Debug/net6.0/Bookstore.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+508d6444d5b34b81098c2d0e85f0c8b3935ce17b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2731f5387fc5f5716798dcb0b4ebbd475f8ff140")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
